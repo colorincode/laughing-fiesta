8c8fd86f78e96f6c8d93e53c2d7baa22d59d6c02
Kayla B <colorincodeco@gmail.com>
2024-08-18 16:34:43 -0600

    discard useless utils

--- src/ts/utils/preload.js
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-// import imagesLoaded from "imagesloaded";
-
-export function preloadImages(selector) {
-	return new Promise((resolve, reject) => {
-		imagesLoaded(selector, { background: true }, resolve);
-	});
-}
